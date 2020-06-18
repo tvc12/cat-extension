4589ab52f1da8fb99d9208fa78efa92028407434
tvc12 - Vi Chi Thien <meomeocf98@gmail.com>
2020-06-18 14:48:28 +0700

    Add chrome extension

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <img src="./assets/logo.png">
+    <HelloWorld/>
   </div>
 </template>
 
-<script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+<script>
+import HelloWorld from './components/HelloWorld';
 
-@Component({
+export default {
+  name: 'App',
   components: {
     HelloWorld,
   },
-})
-export default class App extends Vue {}
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
